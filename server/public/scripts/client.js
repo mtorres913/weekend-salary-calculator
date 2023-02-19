c1109229563f6cbe6947a7b9a8e75178fca4d45c
MIguel Torres <mtorres913@gmail.com>
2023-02-19 14:24:31 -0600

    added background color to high monthly cost. Submitting assignemnt

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,10 @@ console.log('Do not pray for easy lives');
 //Linked Dom elements
 let highCost = 20000
 let salaryTotal = 0
-  // Update the total guesses made
-// // Guess history
-// const guessHistory = {}
-// for (let i = 0 < guessInputs.clientHeight; i++){
-//     guessHistory('players{i+1}') = []
-// }
 
 function submitData(event) {
     // Stop the page from refreshing
     event.preventDefault();
-
     // Find the input field and assign the value to our variable
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
@@ -21,7 +14,6 @@ function submitData(event) {
     let empTitle = document.querySelector('#emp-title').value;
     let empSalary = document.querySelector('#emp-salary').value;
     // Find the tbody on the page so that we can append to it
-    let empData = document.querySelector('#emp-data');
     let monthCost = document.querySelector('#month-cost')
     // if (monthCost > highCost) {
   // finalCost = background color red to month cost
@@ -41,14 +33,11 @@ function submitData(event) {
     `;
     salaryTotal += empSalary/12
     monthCost.innerHTML =  `<p>Monthly Cost: ${salaryTotal}</p>`
-   // totalGuesses.innerHTML = `<p>Total Guesses Made ${currentIndex}</p>` ;
+    if (salaryTotal > highCost) {
+      monthCost.innerHTML = `<p class="high-cost">Monthly Cost: ${salaryTotal}</p>`
+  }
 }
-// Function to restart the game
 function deleteEmp(event) {
   event.target.parentElement.parentElement.remove();
   }
-  // function monthCost(event){
-  //  let monthCost = document.querySelector('#emp-salary'/12);
 
-  //   finalCost.innerHTML = `<p>Monthly Cost: ${monthCost} </p>`;
-  // }
